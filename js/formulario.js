55c6cbe2bd71d4767b11cc33555dcaa048f2d34b
lucas0headshot <lucas0headshot@gmail.com>
2023-05-08 00:18:54 -0300

    Adição das máscaras aos campos Telefone e Celular/Whatsapp
    Ainda preciso verificar se o usuário possui mais de 18 anos e validar o telefone e celular/whatsapp

--- js/formulario.js
+++ js/formulario.js
@@ -1,10 +1,16 @@
 //Função executa ao tentar Enviar o formulário
-$('#form-contato').submit(function(){
+$('#form-contato').submit(function(e){
+    e.preventDefault();
+
     var nome = $('#nome'); //Var nome recebe #nome do Formulário
+    // var dataNasc = new Date($('#dataNasc'));
     var email = $('#email');
     var tel = $('#telefone');
+    var wpp = $('#wpp');
     var msg = $('#mensagem');
     var aviso = $('#aviso');
+
+    // var dataAtual = new Date();
 
     //Adiciona a classe d-none aos avisos, tornando-os invisíveis
     $('#aviso, #aviso-nome, #aviso-email, #aviso-tel, #aviso-msg').addClass('d-none');
@@ -12,13 +18,21 @@ $('#form-contato').submit(function(){
     //Remove a classe is-invalid dos campos
     $('.is-invalid').removeClass('is-invalid');
 
-    if (nome.val() == ''){ //Verifica se Nome está vazio
+    if (nome.val() == ''){ //Verifica se Nome está vazio ou é menor que 3 caracteres
         $('#aviso-nome').removeClass('d-none'); //Remove a classe d-none do #aviso-nome, tornando-o visível
         aviso.removeClass('d-none'); //Remove a classe d-none do #aviso, tornando-o visível
         nome.focus(); //Alterna o foco para o nome
         nome.addClass('is-invalid'); //Adiciona a classe is-invalid, sinalizando o campo
         
     }
+
+    /* if ((dataNasc.getFullYear - dataAtual.getFullYear) < 18){ //Verifica Data Nascimento
+        $('#aviso-data').removeClass('d-none');
+        aviso.removeClass('d-none');
+        dataNasc.focus();
+        dataNasc.addClass('is-invalid');
+        return false;
+    } */
 
     if (email.val() == ''){ //Verifica E-mail
         $('#aviso-email').removeClass('d-none');
@@ -28,11 +42,19 @@ $('#form-contato').submit(function(){
         return false;
     }
 
-    if (tel.val() == ''){ //Verifica Mensagem
+    if (tel.val() == ''){ //Verifica Telefone
         $('#aviso-tel').removeClass('d-none');
         aviso.removeClass('d-none');
         tel.focus();
         tel.addClass('is-invalid');
+        return false;
+    }
+
+    if (wpp.val() == ''){ //Verifica Celular/WhatsApp
+        $('#aviso-wpp').removeClass('d-none');
+        aviso.removeClass('d-none');
+        wpp.focus();
+        wpp.addClass('is-invalid');
         return false;
     }
 
@@ -44,13 +66,7 @@ $('#form-contato').submit(function(){
         return false;
     }
 
-    if ($('#confirmaWhatsapp').is(':checked') == true){ //Verifica se "É WhatsApp" está marcado
-        var wpp = tel.val();
-    }else{
-        var wpp = 0;
-    }
-
-    if ((nome.val() != '') && (email.val() != '') && (tel.val() != '') && (msg.val() != '')){ //Verifica se todos os campos não estão vazios
+    if ((nome.val() != '') && (email.val() != '') && (tel.val() != '') && (wpp.val() != '') && (msg.val() != '')){ //Verifica se todos os campos não estão vazios
         $.ajax({ //Ajax para enviar os dados pro PHP
             url: 'php/inserir.php', //URL do arquivo onde será enviado os dados
             method: 'POST', //Método para enviar
@@ -58,7 +74,7 @@ $('#form-contato').submit(function(){
                 f_nome: nome.val(), 
                 f_email: email.val(), 
                 f_tel: tel.val(), 
-                f_wpp: wpp, 
+                f_wpp: wpp.val(), 
                 f_msg: msg.val()},
             success: function(result){
                 console.log(result);
@@ -73,7 +89,7 @@ $('#form-contato').submit(function(){
             }
         });
         
-        return true;
+        return false;
     }else{
         return false;
     }
